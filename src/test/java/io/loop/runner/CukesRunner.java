ca6d3db89542bffbff41a294a6a7ec155cf28b6b
Akbar <you@example.com>
2025-03-02 10:34:57 -0500

    day13

--- src/test/java/io/loop/runner/CukesRunner.java
+++ src/test/java/io/loop/runner/CukesRunner.java
@@ -4,9 +4,10 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
-@CucumberOptions(
-        plugin ={"html:target/html-reports/cucumber-report.html",
-                 "json:target/json-reports/json-report.json"},
+@CucumberOptions(plugin ={"pretty",
+                "html:target/html-reports/cucumber-report.html",
+                 "json:target/json-reports/json-report.json",
+        "rerun:target/rerun.txt"},
         features ="src/test/resources/features",
         glue = "io/loop/step_definitions",
         dryRun = false,
